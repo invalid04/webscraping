b6c25b1c50a48888c221ebe0b8794ed2002f291b
invalid04 <68178371+invalid04@users.noreply.github.com>
2024-05-24 03:29:21 -0500

    auto upgrade purchase

--- cookieclicker.py
+++ cookieclicker.py
@@ -13,6 +13,8 @@ driver.get("https://orteil.dashnet.org/cookieclicker/")
 
 cookie_id = "bigCookie"
 cookies_id = "cookies"
+product_price_prefix = "productPrice"
+product_prefix = "product"
 
 WebDriverWait(driver, 5).until(
     EC.presence_of_element_located((By.XPATH, "//*[contains(text(), 'English')]"))
@@ -31,4 +33,16 @@ while True:
     cookie.click()
     cookies_count = driver.find_element(By.ID, cookies_id).text.split(" ")[0]
     cookies_count = int(cookies_count.replace(",", ""))
-    print(cookies_count)
+    
+    for i in range(4):
+        product_price = driver.find_element(By.ID, product_price_prefix + str(i)).text.replace(",", "")
+
+        if not product_price.isdigit():
+            continue
+
+        product_price = int(product_price)
+
+        if cookies_count >= product_price:
+            product = driver.find_element(By.ID, product_prefix + str(i))
+            product.click()
+            break
